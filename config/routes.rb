0f06c2b86a494a8e7769a77173cce7ba7d5f525e
Jorge Madrid <jorjilio@hotmail.com>
2014-11-12 21:12:55 -0500

    agregando vista de contacto con mapa

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get 'static_pages/services'
   get 'static_pages/faq'
   get 'static_pages/about'
+  get 'static_pages/contact'
 
   resources :pets
   resources :institutions
